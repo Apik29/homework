f9018890436ed39c9da10f4508cdea6662a0fbb2
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-01-17 16:56:02 +0300

    homework
    Nice

---
+++ work1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Добро пожаловать в программу,  которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее");
+        Console.WriteLine("Введите первое число");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        if (number1 > number2)
+        {
+            Console.WriteLine("Первое число " + number1 + " больше чем второе " + number2);
+        }
+        else
+        {
+            Console.WriteLine("Второе число " + number2 + " больше чем первое " + number1);
+        }
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
