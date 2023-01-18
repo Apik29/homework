295e8599219172858effbc1434c39df570389ef7
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-01-18 14:25:30 +0300

    3
    3 ok

---
+++ work3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Добро пожаловать в программу, которая на вход принимает число и выдаёт, является ли число чётным");
+Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 1)
+{
+    Console.WriteLine( "Число " + number1 + "является: Нечетным" ) ;
+}
+    else
+{
+    Console.WriteLine("Число " + number1 + "является: Четным") ;
+}
